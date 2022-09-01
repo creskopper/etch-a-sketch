d336ca1324026e2dceac9de360bbcf12e9693ece
Cresk <creskopper@gmail.com>
2022-09-01 10:46:12 +0100

    Create new grid based on user input
    Add corresponding functions for creating grid, removing grid,
    painting grid, and adding css grid template to the grid.
    Separate initial square per side variable from user input variable.

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const container = document.querySelector(".container");
 const divsDocumentFragment = new DocumentFragment();
-let squaresPerSide = 20;
+let initialGrid = 16;
+let squaresPerSide = 0;
 let numberOfDivs = squaresPerSide * squaresPerSide;
-
-container.style.gridTemplateColumns = `repeat(${squaresPerSide}, auto)`;
-container.style.gridTemplateRows = `repeat(${squaresPerSide}, auto)`;
-
-createGridSquare();
-
-function createGridSquare() {
-    for(i = 0; i < numberOfDivs; i++) {
-        div = document.createElement("div");
-        div.classList.add("square");
-        divsDocumentFragment.appendChild(div);
-    }
-    container.appendChild(divsDocumentFragment);
-}
-
-let squaresList = document.querySelectorAll(".square");
+const newGridBtn = document.getElementById("new-grid");
 let isDown = false;
 
+createGridSquare(initialGrid);
+addGridTemplate(initialGrid);
+paintGrid();
+
+newGridBtn.addEventListener("click", () => {
+    removeGrid();
+    squaresPerSide = Number(prompt("Enter squares per side!"));
+    createGridSquare(squaresPerSide);
+    addGridTemplate(squaresPerSide);
+    paintGrid(squaresPerSide);
+});
 container.addEventListener("mousedown", () => {
     isDown = true;
 });
@@ -30,17 +27,42 @@ container.addEventListener("mouseleave", () => {
     isDown = false;
 });
 
-squaresList.forEach(square => {
-    square.addEventListener("mouseover", (e) => {
-        if(!isDown) return;
-        e.preventDefault();
-        square.classList.add("square-colored");
-    })
-});
+function createGridSquare(side) {
+    for(i = 0; i < (side ** 2); i++) {
+        div = document.createElement("div");
+        div.classList.add("square");
+        divsDocumentFragment.appendChild(div);
+    }
+    container.appendChild(divsDocumentFragment);
+}
 
-squaresList.forEach(square => {
-    square.addEventListener("mousedown", (e) => {
-        e.preventDefault();
-        square.classList.add("square-colored");
+function paintGrid() {
+    squaresList = document.querySelectorAll(".square");
+
+    squaresList.forEach(square => {
+        square.addEventListener("mouseover", (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            square.classList.add("square-colored");
+        })
+    });
+    squaresList.forEach(square => {
+        square.addEventListener("mousedown", (e) => {
+            e.preventDefault();
+            square.classList.add("square-colored");
+        })
+    });
+}
+
+function removeGrid() {
+    squaresList = document.querySelectorAll(".square");
+    squaresList.forEach(square => {
+        container.removeChild(square);
     })
-});
+}
+
+function addGridTemplate(init) {
+    container.style.gridTemplateColumns = `repeat(${init}, auto)`;
+    container.style.gridTemplateRows = `repeat(${init}, auto)`;
+
+}
